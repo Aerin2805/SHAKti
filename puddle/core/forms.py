018c2e99a7b1fc026b0405bebcf4d305c6c0dcf4
Aerin2805 <aerinpatel28@gmail.com>
2024-02-17 08:34:57 +0530

    Singup , login page and form submission error css updated

--- puddle/core/forms.py
+++ puddle/core/forms.py
@@ -1,30 +1,29 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm , AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+
+INPUT_CLASSES = 'w-full py-4 px-6 rounded-xl'
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(
-        widget=forms.TextInput(attrs={'placeholder': 'Your username', 'class': 'w-full py-4 px-6 rounded-xl'})
+        widget=forms.TextInput(attrs={'placeholder': 'Your username', 'class': INPUT_CLASSES, 'style': 'color: white; background-color: #1a202c;'})
     )
     password = forms.CharField(
-        widget=forms.PasswordInput(attrs={'placeholder': 'Your password', 'class': 'w-full py-4 px-6 rounded-xl'})
+        widget=forms.PasswordInput(attrs={'placeholder': 'Your password', 'class': INPUT_CLASSES, 'style': 'color: white; background-color: #1a202c;'})
     )
-
-
-
 
 class SignupForm(UserCreationForm):
     username = forms.CharField(
-        widget=forms.TextInput(attrs={'placeholder': 'Your username', 'class': 'w-full py-4 px-6 rounded-xl'})
+        widget=forms.TextInput(attrs={'placeholder': 'Your username', 'class': INPUT_CLASSES, 'style': 'color: white; background-color: #1a202c;'})
     )
     email = forms.EmailField(
-        widget=forms.EmailInput(attrs={'placeholder': 'Your Email address', 'class': 'w-full py-4 px-6 rounded-xl'})
+        widget=forms.EmailInput(attrs={'placeholder': 'Your Email address', 'class': INPUT_CLASSES, 'style': 'color: white; background-color: #1a202c;'})
     )
     password1 = forms.CharField(
-        widget=forms.PasswordInput(attrs={'placeholder': 'Your password', 'class': 'w-full py-4 px-6 rounded-xl'})
+        widget=forms.PasswordInput(attrs={'placeholder': 'Your password', 'class': INPUT_CLASSES, 'style': 'color: white; background-color: #1a202c;'})
     )
     password2 = forms.CharField(
-        widget=forms.PasswordInput(attrs={'placeholder': 'Repeat password', 'class': 'w-full py-4 px-6 rounded-xl'})
+        widget=forms.PasswordInput(attrs={'placeholder': 'Repeat password', 'class': INPUT_CLASSES, 'style': 'color: white; background-color: #1a202c;'})
     )
 
     class Meta:
